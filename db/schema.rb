b4bf337ab7575bf3d435906652d9bab9dd3425af
Nacho521 <juanignacioromero123456@gmail.com>
2022-05-05 10:56:49 -0300

    other changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_05_114418) do
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
