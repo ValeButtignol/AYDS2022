87b9b89b6a1fd9e00cb62f305d611cfcb9747a73
Nacho521 <juanignacioromero123456@gmail.com>
2022-05-12 08:12:49 -0300

    Modificed test

--- spec/Forecast_spec.rb
+++ spec/Forecast_spec.rb
@@ -1,12 +1,12 @@
-require_relative '../app/models/forecast.rb'
+require_relative '../app/models/init.rb'
 
 describe 'Forecast' do 
     describe 'when the number is negative for the home' do
         it 'It is not valid' do
             
-            f = Forecast.new(home_goals: 4)
+            f = Forecast.new(home_goals: -4)
 
-            expect(f.home_goals()).to be > 0
+            expect(f.home_goals()).to be < 0
         end
 
     end
@@ -14,8 +14,8 @@ describe 'Forecast' do
     describe 'when the number is negative for the visitor' do
         it 'It is not valid' do
 
-        f = Forecast.new(visitor_goals: 3)
-        expect(f.visitor_goals()).to be > 0
+        f = Forecast.new(visitor_goals: -4)
+        expect(f.visitor_goals()).to be < 0
         
         end
     end   
