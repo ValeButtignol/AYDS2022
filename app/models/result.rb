115bfd323a3ff1fa97246694a9b702b401f38fee
JoseCompag <joseic2000@gmail.com>
2022-05-18 23:18:50 -0300

    points function added

--- app/models/result.rb
+++ app/models/result.rb
@@ -8,6 +8,22 @@ class Result < ActiveRecord::Base
     self.winner = set_winner(self.home_goals, self.visitor_goals) 
   end
   
+    # Callback that sets the points of the players automaticly.
+  before_create do
+    self.match.forecasts.each do |f|
+      set_points(self, f)
+    end
+  end
+
+  # Function that calculates the winner.
+  def set_points(result, forecast)
+    if result.winner == forecast.winner then
+      forecast.player.update(points: forecast.player.points + 2)
+      if result.home_goals == forecast.home_goals and result.visitor_goals == forecast.visitor_goals then
+        forecast.player.update(points: forecast.player.points + 1)
+      end
+    end
+  end
 
   def set_winner(home_goals, visitor_goals)
     if home_goals > visitor_goals
