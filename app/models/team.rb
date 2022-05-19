3b7b2e8c5b13ce6361f8d0fe6aa39144816409e8
JoseCompag <joseic2000@gmail.com>
2022-05-18 23:22:36 -0300

    empty layout added

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ActiveRecord::Base
   
   has_many :local_matches, foreign_key: 'local_id', class_name: 'Match'
   has_many :visitor_matches, foreign_key: 'visitor_id', class_name: 'Match'
+  has_many :administrators
   
-  validates :name , presence: true
   
 end
