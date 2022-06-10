773d0c04890df8fa8f08f5f9ae95216281897a83
Vale Buttignol <buttignolv21@gmail.com>
2022-06-10 00:50:27 -0300

    Refactoring code

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -96,7 +96,7 @@ class App < Sinatra::Application
 
   patch '/admin/group_edit/:id' do
     Group.find_by(id: params[:id]).update(name: params['name'])
-    redirect to '/landingpage_admin'
+    redirect to '/admin/landingpage'
   end
 
   delete '/admin/group_delete/:id' do
@@ -133,17 +133,17 @@ class App < Sinatra::Application
     end
   end
 
-  get '/admin/teams/:id/edit' do
+  get '/admin/team/:id/edit' do
     @team = Team.find_by(id: params[:id])
     erb :'teams/edit_team'
   end
 
-  patch '/admin/teams_edit/:id' do
+  patch '/admin/team_edit/:id' do
     Team.find_by(id: params[:id]).update(name: params['name'], group_id: params['group_id'], administrator_id: params['admin_id']) 
-    redirect to '/all_teams'
+    redirect to '/admin/landingpage'
   end
 
-  delete '/admin/teams_delete/:id' do
+  delete '/admin/team_delete/:id' do
     @team = Team.find_by(id: params[:id])
     @matches = Match.all
     had_match = false
@@ -163,7 +163,7 @@ class App < Sinatra::Application
   end
 
   get '/admin/groups&teams' do
-    erb :'teams/all_teams'
+    erb :'teams/groups_teams'
   end
 
 ################## RESULTS CONTROLLERS ##################
@@ -283,11 +283,11 @@ class App < Sinatra::Application
 
 ################## MATCHES CONTROLLERS ##################
 
-  get '/create_match' do
+  get '/admin/match/new' do
     erb :'matches/create_match' 
   end
 
-  post '/create_match' do
+  post '/admin/match/new' do
     match = Match.new
     match.administrator_id = session[:admin_id]
     match.stadium = params['stadium']
@@ -301,40 +301,39 @@ class App < Sinatra::Application
     logger.info(session)
    
     if match.save then
-      redirect to '/landingpage_admin'
+      redirect to '/admin/landingpage'
     else
-      redirect '/create_match'
+      redirect '/admin/match/new'
     end
   end
   
-  get '/all_matches/:id/edit' do
+  get '/admin/match/:id/edit' do
     @match = Match.find_by(id: params[:id])
     erb :'matches/edit_match'
   end
   
-  patch '/all_matches_edit/:id' do
+  patch '/admin/match_edit/:id' do
     Match.find_by(id: params[:id]).update(home_team_id: params['home_team_id'],visitor_team_id: params['visitor_team_id'],stadium: params['stadium'], date: params['date'], match_type: params['match_type'])
-    redirect to '/all_matches'
+    redirect to '/admin/matches'
   end
   
-  delete '/all_matches_delete/:id' do
+  delete '/admin/match_delete/:id' do
     @match = Match.find_by(id: params[:id])
     if (@match.forecasts.count == 0 && @match.result == nil)
       @match.destroy
-      redirect to '/landingpage_admin'
+      redirect to '/admin/landingpage'
     else
       #Flash message
-      redirect to '/all_matches'
+      redirect to '/admin/matches'
     end  
   end
 
-  get '/all_matches' do
-    erb :'matches/all_matches_admin'
+  get '/admin/matches' do
+    erb :'matches/matches_admin'
   end
 
   # Show all the not played matches and the matches to play.
-  get '/matches' do
-    erb :'matches/all_matches'
+  get '/player/matches' do
+    erb :'matches/matches_player'
   end
-
 end
