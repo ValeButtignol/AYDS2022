287e4212152a3a1e5344e82a2ba069820d702724
Nacho521 <juanignacioromero123456@gmail.com>
2022-06-14 19:08:36 -0300

    Add methods of result

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -208,23 +208,11 @@ class App < Sinatra::Application
 ################ RESULTS CONTROLLERS ################
 
   get '/admin/result/new' do
-    erb :'results/create_result'
+    get_result
   end
 
   post '/admin/result/new' do
-  result = Result.new
-  result.home_goals = params['home_goals']
-  result.visitor_goals = params['visitor_goals']
-  result.match_id = params['match_id']
-  result.administrator_id = session[:admin_id]
+    post_result
+  end
 
-  #Flash message: Are you sure you want to create this result?
-  logger.info(result)
-  if result.save then
-    redirect to '/admin/landingpage'
-  else
-    redirect '/admin/result/new' 
-  end
-  end
-  
 end
