b7b9702a528facf9bb85f7fa749a1df75d2363dd
Nacho521 <juanignacioromero123456@gmail.com>
2022-06-14 18:25:46 -0300

    Add helpers and add init for the server

---
+++ app/helpers/matches_helper.rb
@@ -0,0 +1,60 @@
+class  MatchesController < Sinatra::Base
+
+  configure do
+    set :views, 'app/views'
+  end
+    
+  get '/admin/match/new' do
+        erb :'matches/create_match' 
+  end
+
+  post '/admin/match/new' do
+    match = Match.new
+    match.administrator_id = session[:admin_id]
+    match.stadium = params['stadium']
+    match.date = params['date']
+    match.home_team_id = params['home_team_id']
+    match.visitor_team_id = params['visitor_team_id']
+    match.match_type = params['match_type']
+
+    # Flash message: Are you sure you want to create this match?
+
+    logger.info(session)
+   
+    if match.save then
+      redirect to '/admin/landingpage'
+    else
+      redirect '/admin/match/new'
+    end
+  end
+  
+  get '/admin/match/:id/edit' do
+    @match = Match.find_by(id: params[:id])
+    erb :'matches/edit_match'
+  end
+  
+  patch '/admin/match_edit/:id' do
+    Match.find_by(id: params[:id]).update(home_team_id: params['home_team_id'],visitor_team_id: params['visitor_team_id'],stadium: params['stadium'], date: params['date'], match_type: params['match_type'])
+    redirect to '/admin/matches'
+  end
+  
+  delete '/admin/match_delete/:id' do
+    @match = Match.find_by(id: params[:id])
+    if (@match.forecasts.count == 0 && @match.result == nil)
+      @match.destroy
+      redirect to '/admin/landingpage'
+    else
+      #Flash message
+      redirect to '/admin/matches'
+    end  
+  end
+
+  get '/admin/matches' do
+    erb :'matches/matches_admin'
+  end
+
+  # Show all the not played matches and the matches to play.
+  get '/player/matches' do
+    erb :'matches/matches_player'
+  end
+end
---
+++ app/helpers/players_helper.rb
@@ -0,0 +1,46 @@
+module Players
+  
+  def get_signup
+    erb :'/players/signup'
+  end
+
+  def post_signup
+    # Recieve data from the form inside of params hash.
+    # Create a new player and persist it.
+  player = Player.new(params)
+        
+    if player.save then
+      redirect to '/player/login'
+    else
+      # Flash message: username invalid
+      redirect to '/player/signup'
+    end
+
+  end
+    
+  def get_login
+    erb :'players/login'
+  end
+    
+  def post_login
+    player = Player.find_by(username: params[:username])
+  
+    if player && player.authenticate(params[:password])
+      session[:player_id] = player.id 
+      redirect to '/player/landingpage'
+    else
+      # Flash message: Username or password invalid
+      redirect '/player/login'
+    end
+  end
+  
+  def get_logout
+    session.clear
+    redirect to '/'
+  end
+  
+  def get_landingpage
+    erb :'players/landingpage'
+  end
+   
+end
