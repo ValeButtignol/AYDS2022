168bd40ad0c76a508940c08520f95b4aac5d2bc1
Nacho521 <juanignacioromero123456@gmail.com>
2022-09-12 12:33:21 -0300

    Add require relative

---
+++ app/helpers/init_helper.rb
@@ -0,0 +1,15 @@
+require_relative '../helpers/players_helper'
+require_relative '../helpers/administrators_helper'
+require_relative '../helpers/teams_helper'
+require_relative '../helpers/groups_helper'
+require_relative '../helpers/forecasts_helper'
+require_relative '../helpers/results_helper'
+require_relative '../helpers/matches_helper'
+
+helpers Players
+helpers Administrators
+helpers Forecasts
+helpers Teams
+helpers Groups
+helpers Results
+helpers Matches
